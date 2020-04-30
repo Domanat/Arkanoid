ce1df81aaef65b75d0709b1b9b04fced92d4b176
Domanat <maxim.tomilko@yandex.ru>
2020-04-30 20:15:33 +0300

    Abstract class for all objects

---
+++ Entity.hpp
@@ -0,0 +1,27 @@
+#ifndef ENTITY_HPP
+#define ENTITY_HPP
+
+#include <SFML/Graphics.hpp>
+
+class Entity : public sf::Drawable
+{
+protected:
+	sf::Texture texture;
+	sf::Sprite sprite;
+
+	float dx, dy;
+	bool onGround;
+	float frameNumber;
+
+	sf::FloatRect rect;
+
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const = 0;
+	
+	virtual sf::Sprite& getSprite() = 0;
+	virtual sf::FloatRect& getRect() = 0;
+
+	virtual void update(float dt) = 0;;
+
+};
+
+#endif
